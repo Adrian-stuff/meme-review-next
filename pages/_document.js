b7002321283fe84dedd5687fb562d12e6964dcf2
Adrian-stuff <mahalkosariliko6@gmail.com>
2021-06-16 22:20:47 +0800

    added adsense link

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            data-ad-client="ca-pub-6496118406995290"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
+        </Head>
         <body className="dark:bg-gray slowInOut dark:text-white text-black antialiased font-body">
           <Main />
           <NextScript />
